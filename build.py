30770cf1f136e34350a633ae28b71dd7aed830bc
CB26051970 <cristiano.bianchini.gsd@gmail.com>
2025-11-04 11:31:55 +0100

    Update build.py

--- build.py
+++ build.py
@@ -3,25 +3,19 @@ import os
 import sys
 
 def build_executable():
-    # Determina il percorso dello script principale
-    main_script = os.path.join('src', 'main.py')
-    
     # Parametri per PyInstaller
     params = [
-        main_script,
+        'src/main.py',
         '--name=SparConverter',
         '--onefile',
         '--windowed',
         '--hidden-import=pandas',
         '--hidden-import=openpyxl',
         '--hidden-import=tkinter',
+        '--hidden-import=openpyxl.workbook',
         '--collect-all=pandas',
         '--collect-all=openpyxl',
     ]
-    
-    # Aggiungi icona se presente
-    if os.path.exists('icon.ico'):
-        params.append('--icon=icon.ico')
     
     PyInstaller.__main__.run(params)
 
